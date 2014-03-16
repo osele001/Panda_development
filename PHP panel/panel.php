368b157116fcdc4156cd3a45cdaf6e0f83f3fd67
Bazanov Ilia <flarework@yandex.ru>
2014-03-16 16:01:28 +0200

    Little bit of defence when logging
    + few bugs deleted

--- PHP panel/panel.php
+++ PHP panel/panel.php
@@ -4,54 +4,53 @@
 <link rel="stylesheet" href="style.css" type="text/css" />       
 <title>Panda development project</title>
 </head>
-
-<?php
-	include "conf.php";
-	session_start();
-	
-	//Подключаемся к базе данных.
-	$dbcon = mysql_connect($base_name, $base_user, $base_pass); 
-	mysql_select_db($db_name, $dbcon);
-	if (!$dbcon)
-	{
-		echo "<p>Error in connecting to MySQL!</p>".mysql_error(); exit();
-	} else {
-		if (!mysql_select_db($db_name, $dbcon))
-		{
-			echo("<p>Selected DB is not existing!</p>");
-		}
-	}
-				
-	mysql_query("SET NAMES 'utf8'");
-?>
-           
-</head>
-
+  
 <body>
 
 <div id="upper_menu"> 
 	<ul>
-		<a href="panel.php"><li class="selected">Button 1</li></a>
-		<a href="panel.php"><li>Button 2</li></a>
-		<a href="panel.php"><li>Button 3</li></a>
-		<a href="panel.php"><li>Button 4</li></a>
-		<a href="panel.php"><li>Button 5</li></a>
+		<a href="panel.php"><li class="selected">My orders</li></a>
+		<a href="panel.php"><li>Statistics</li></a>
+		<a href="panel.php"><li>Products</li></a>
 	</ul>
 </div>
 
 
 <?php
 	
+	session_start();
+	
 	if((isset($_SESSION["email"])))
 	{
 		$email = $_SESSION["email"];
 		$hash = $_SESSION["hash"];
+		
+		include "conf.php";
+		
+		//Подключаемся к базе данных.
+		
+		$dbcon = mysql_connect($base_name, $base_user, $base_pass); 
+		mysql_select_db($db_name, $dbcon);
+		if (!$dbcon)
+		{
+			echo "<p>Error in connecting to MySQL!</p>".mysql_error(); exit();
+		} else {
+			if (!mysql_select_db($db_name, $dbcon))
+			{
+				echo("<p>Selected DB is not existing!</p>");
+			}
+		}
+					
+		mysql_query("SET NAMES 'utf8'");
 		
 		$result = mysql_query("SELECT * FROM ultra_secured_table_of_users WHERE email='$email'",$dbcon);
 		$myrow = mysql_fetch_array($result);
 		
 		if($myrow["hash"] == $hash)
 		{
+		
+		
+		
 			// только теперь работаем
 		
 			echo("<div id='alert_box'>");
@@ -65,7 +64,9 @@
 		
 		
 		
-		}		
+		}
+		else
+		echo("Sorry, login or password is wrong");
 	}
 	else
 		echo("Sorry, you need to re-login");
